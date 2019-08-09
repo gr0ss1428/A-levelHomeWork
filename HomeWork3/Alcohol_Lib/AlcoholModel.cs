77ca00c5631d7799558c4de0b781d6fa9ac1d215
gr0ss1428 <gr0ss@sktbpe.com>
2019-08-09 16:00:37 +0300

    HomeWork3

---
+++ HomeWork3/Alcohol_Lib/AlcoholModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alcohol_Lib
+{
+    public abstract class AlcoholModel
+    {
+        public AlcoholModel(string types, string name, string manufacrurer, uint abv, uint kCal)
+        {
+            Types = types;
+            Name = name;
+            Manufacturer = manufacrurer;
+            AbvPercent = abv;
+            KCal = kCal;
+        }
+        public string Types { get; }
+        public string Name { get; }
+        public string Manufacturer { get; }
+        public uint AbvPercent { get; }
+        public uint KCal { get; }
+    }
+    public class AlcoholBottle : AlcoholModel
+    {
+        
+        public AlcoholBottle(string types, string name, string manufacrurer, uint abv, uint kCal, uint volume, uint quantity,uint price) : base(types, name, manufacrurer, abv, kCal)
+        {
+            Volume = volume;
+            Quantity = quantity;
+            Prise = price;
+
+        }
+        public uint Volume { get; }
+        public uint Quantity { get; private set; }
+        public uint Prise { get; set; }
+        public uint BayBottle(uint quantity= 1)
+        {
+            uint bottleBay = 0;
+            if (quantity > this.Quantity)
+            {
+                    bottleBay = this.Quantity;
+                    this.Quantity = 0;
+            }
+            else
+            {
+                bottleBay = quantity;
+                this.Quantity -= quantity;
+            }
+            return bottleBay;
+        }
+        public void AddBottle(uint quantity = 1)
+        {
+            Quantity += quantity;
+        }
+    }
+}
---
+++ HomeWork3/BuhlishkoBase_Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alcohol_Lib;
+namespace BuhlishkoBase_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            bool exit = false;
+            List<string> mainMenu = new List<string>() { "1)Добавить новый товар.","2)Удалить товар.","3)Добавить новый вид алкоголя.","4)Показать склад.","5)Изменить количество товара.","6)Выход." };
+            int selectedMenu;
+            do
+            {
+               
+                do
+                {
+                    Console.Clear();
+                    PrintMenu("Добро пожаловать на склад Бухлишко",mainMenu);
+                    while (!int.TryParse(Console.ReadLine(), out selectedMenu))
+                    {
+                        ClearLine(1);
+                        Console.Write($"Selected (1-{mainMenu.Count}):");
+                    }
+                } while (!(selectedMenu >= 1 && selectedMenu <= mainMenu.Count));
+
+                switch(selectedMenu)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    case 6:
+                        exit = true;
+                        break;
+                }
+
+            } while(!exit);
+        }
+        static void PrintMenu(string message,List<string> menu)
+        {
+            Console.WriteLine(message);
+            foreach (var m in menu)
+            {
+                Console.WriteLine(m);
+            }
+            Console.Write("Selected (1-{0}):", menu.Count);
+
+
+        }
+        static void ClearLine(int clear=0)
+        {
+            int cursor = Console.CursorTop - clear;
+            Console.SetCursorPosition(0, cursor);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, cursor);
+        }
+    }
+}
