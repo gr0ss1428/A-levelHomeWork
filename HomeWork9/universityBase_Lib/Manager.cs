9e508f1e23fed7808d647b326a88cf5f5d60a339
kolesoxaxol <dp260793kev@yandex.ua>
2019-09-08 08:36:42 +0300

    review

--- HomeWork9/universityBase_Lib/Manager.cs
+++ HomeWork9/universityBase_Lib/Manager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,10 @@ namespace universityBase_Lib
 {
     public class Manager
     {
-        Dictionary<uint ,Group> groups;
+        Dictionary<uint, Group> groups;
         List<Teacher> teachers;
         List<Student> students;
+        
         public int CurrentCount
         {
             get
@@ -18,12 +19,14 @@ namespace universityBase_Lib
                 return groups.Count;
             }
         }
+        
         public Manager()
         {
             groups = new Dictionary<uint, Group>();
             students = new List<Student>();
             teachers = new List<Teacher>();
         }
+        
         public bool NewGroup(uint number,Teacher teacher)
         {
             if (!groups.ContainsKey(number))
@@ -33,10 +36,12 @@ namespace universityBase_Lib
             }
             return false;
         }
+        
         public void NewTeaher(Teacher teacher)
         {
             teachers.Add(teacher);
         }
+        
         public bool NewStudent(Student student)
         {
             if(groups.ContainsKey(student.Group))
@@ -47,23 +52,35 @@ namespace universityBase_Lib
             }
             return false;
         }
+        
         public uint [] GetGroups()
         {
             return groups.Keys.ToArray();
         }
+        
         public Group[] GetGroupsOb()
         {
             return groups.Values.ToArray();
         }
+        
         public IEnumerable<Teacher> GetTeacherArr(string pos="ВСЕ")
         {
+            // если юзаешь елсе обварачивай в фигурные скобки - хороший тон. Тернарный оператор бы тут юзал 
             if (pos =="ВСЕ") return teachers.ToArray();
             else return teachers.ToArray().Where((t) => t.Position == pos);
         }
-        public IEnumerable<Student> GetStudentsArr(object group)
+        
+        public IEnumerable<Student> GetStudentsArr(object group)  // плохая идея юхать тут оbject нельзя например использховать для всех груп 0 или хотябы сделать тут nullable type
         {
-            if( group=="ВСЕ")  return students.ToArray();
-            else return students.ToArray().Where((g)=>g.Group==(uint)group);
+            if(group=="ВСЕ") // ты в этот метод можешь отправить че угодно упадешь тут например если не сможешь кастануть к стринге
+            {
+                return students.ToArray();
+            }            
+            else 
+            {
+                var ugroup = (uint)group;  // тут тоже можешь упасть 
+                return students.ToArray().Where(g => g.Group == ugroup);
+            }
         }
         public bool ValidNumGroup(uint num)
         {
