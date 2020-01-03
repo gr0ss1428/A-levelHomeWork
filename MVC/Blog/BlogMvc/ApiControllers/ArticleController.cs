8b8e4f2b29d76eafb8974607a4591b36ed4d6ca2
gr0ss <gr0ss1428@gmail.com>
2020-01-03 16:56:19 +0200

    blogapi

---
+++ MVC/Blog/BlogMvc/ApiControllers/ArticleController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BlogMvc.Tools;
+using BlogMvc.Models;
+using BlogBl.BlModel;
+using BlogBl;
+
+namespace BlogMvc.ApiControllers
+{
+    public class ArticleController : ApiController
+    {
+        private readonly IService<ArticleBl> _articleService;
+
+        public ArticleController()
+        {
+            _articleService = new ArticleService<ArticleBl>();
+        }
+        // GET: api/Article
+        public IEnumerable<DetailsModel> GetAllArticle()
+        {
+            // return new string[] { "value1", "value2" };
+            var artCollection = MapperTools.MapToCollection<ArticleBl, DetailsModel>(_articleService.GetAll());
+            return artCollection;
+        }
+
+        // GET: api/Article/5
+        public DetailsModel GetArticle(int id)
+        {
+            var res = MapperTools.MapTo<ArticleBl, DetailsModel>(_articleService.GetById(id));
+            return res;
+        }
+
+        // POST: api/Article
+        [HttpPost]
+        public void CreateArticle(DetailsModel item)
+        {
+            item.DateTime = DateTime.Now;
+            item.AuthorId = 1;
+            _articleService.Add(MapperTools.MapTo<DetailsModel, ArticleBl>(item));
+        }
+
+        // PUT: api/Article/5
+        [HttpPut]
+        public void UpdateArticle(int id, DetailsModel item)
+        {
+            _articleService.Update(MapperTools.MapTo<DetailsModel, ArticleBl>(item));
+        }
+
+        // DELETE: api/Article/5
+        [HttpDelete]
+        public void DeleteArticle(int id)
+        {
+            _articleService.Delete(id);
+        }
+    }
+}
